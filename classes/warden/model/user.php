5eb4f86decb6ad78f71b52a22d4a180972919211
Jesse O'Brien <Destroyer661@gmail.com>
2011-10-26 01:32:42 -0400

    Fixed the logic on is_confirm() and is_lock_expired(), they were the reverse of what they should be.

--- classes/warden/model/user.php
+++ classes/warden/model/user.php
@@ -477,7 +477,7 @@ SQL;
      */
     public function is_confirmation_required()
     {
-        return !$this->is_confirmed();
+        return $this->is_confirmed();
     }
 
     /**
@@ -660,7 +660,7 @@ SQL;
      */
     public function is_access_locked()
     {
-        return !$this->is_lock_expired();
+        return $this->is_lock_expired();
     }
 
     /**
