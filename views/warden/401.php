48302db1a54d664ccb3f6220e415910af1c3ce8f
Andrew <lifeandcoding@gmail.com>
2012-02-29 14:52:48 +0300

    updated doctype to html5

--- views/warden/401.php
+++ views/warden/401.php
@@ -1,5 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
- "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<!doctype html>
 <html>
     <head>
         <title>401 Unauthorized</title>
