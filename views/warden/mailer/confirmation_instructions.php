c7bcdf2f1a41f24c4b4438c8060871e0ebd9a0ea
Andrew <lifeandcoding@gmail.com>
2012-03-06 02:41:04 +0300

    undo error in last commit

--- views/warden/mailer/confirmation_instructions.php
+++
@@ -1,9 +0,0 @@
-<p style="font-size: 18px;">Hi <?php echo $username;?>!</p>
-
-<p>You can confirm your account through the link below:</p>
-
-<p>
-    <a href="<?php echo $uri; ?>">
-        Confirm my account
-    </a>
-</p>
