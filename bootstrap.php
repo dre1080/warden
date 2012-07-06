173bb9ced6423c4339d1e9a4aa5650db19e0b4fc
Ando <lifeandcoding@gmail.com>
2012-07-06 23:53:29 +0300

    added basic dependency checking

--- bootstrap.php
+++ bootstrap.php
@@ -10,26 +10,31 @@
  * @copyright  (c) 2011 - 2012 Andrew Wayne
  */
 
+/*
+ * Make sure the dependency packages are loaded.
+ */
+Package::load(array('orm', 'email'));
+
 Autoloader::add_core_namespace('Warden');
 
 Autoloader::add_classes(array(
 	// PHPass
 	'PasswordHash' => __DIR__.'/vendor/phpass/PasswordHash.php',
 	
-    // Base Warden classes
-    'Warden\\Warden'              => __DIR__.'/classes/warden.php',
-    'Warden\\Warden_Driver'       => __DIR__.'/classes/warden/driver.php',
-    'Warden\\Warden_Mailer'       => __DIR__.'/classes/warden/mailer.php',
-    'Warden\\Warden_Failure'      => __DIR__.'/classes/warden/exceptions.php',
-    'Warden\\Warden_AccessDenied' => __DIR__.'/classes/warden/exceptions.php',
+  // Base Warden classes
+  'Warden\\Warden'              => __DIR__.'/classes/warden.php',
+  'Warden\\Warden_Driver'       => __DIR__.'/classes/warden/driver.php',
+  'Warden\\Warden_Mailer'       => __DIR__.'/classes/warden/mailer.php',
+  'Warden\\Warden_Failure'      => __DIR__.'/classes/warden/exceptions.php',
+  'Warden\\Warden_AccessDenied' => __DIR__.'/classes/warden/exceptions.php',
 
-    // Models
-    'Warden\\Model_User'          => __DIR__.'/classes/warden/model/user.php',
-    'Warden\\Model_Profile'       => __DIR__.'/classes/warden/model/profile.php',
-    'Warden\\Model_Role'          => __DIR__.'/classes/warden/model/role.php',
-    'Warden\\Model_Permission'    => __DIR__.'/classes/warden/model/permission.php',
-    'Warden\\Model_Service'       => __DIR__.'/classes/warden/model/service.php',
-    'Warden\\Observer_Timestamps' => __DIR__.'/classes/warden/observer/timestamps.php',
+  // Models
+  'Warden\\Model_User'          => __DIR__.'/classes/warden/model/user.php',
+  'Warden\\Model_Profile'       => __DIR__.'/classes/warden/model/profile.php',
+  'Warden\\Model_Role'          => __DIR__.'/classes/warden/model/role.php',
+  'Warden\\Model_Permission'    => __DIR__.'/classes/warden/model/permission.php',
+  'Warden\\Model_Service'       => __DIR__.'/classes/warden/model/service.php',
+  'Warden\\Observer_Timestamps' => __DIR__.'/classes/warden/observer/timestamps.php',
 ));
 
 Config::load('warden', true);
