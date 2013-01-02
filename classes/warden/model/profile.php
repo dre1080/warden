aab4558da5a40e169fb57246f1c8553762b8e019
Andreo Vieira <andreoav@gmail.com>
2013-01-02 15:49:00 -0200

    SisCBO specific code

--- classes/warden/model/profile.php
+++ classes/warden/model/profile.php
@@ -19,64 +19,55 @@ namespace Warden;
  */
 class Model_Profile extends \Orm\Model
 {
-  /**
-   * Validation regular expression for a name
-   */
-  // const REGEX_NAME = '\^[^;]+$';
+	/**
+	 * Validation regular expression for a name
+	 */
+	// const REGEX_NAME = '\^[^;]+$';
 
-  /**
-   * Belongs to; relationship properties
-   *
-   * @var array
-   */
-  protected static $_belongs_to = array(
-    'user' => array(
-      'key_from'       => 'user_id',
-      'model_to'       => 'Model_User',
-      'key_to'         => 'id',
-      'cascade_save'   => true,
-      'cascade_delete' => true
-    )
-  );
+	/**
+	 * Belongs to; relationship properties
+	 *
+	 * @var array
+	 */
+	protected static $_belongs_to = array(
+		'user' => array(
+			'key_from'       => 'user_id',
+			'model_to'       => 'Model_User',
+			'key_to'         => 'id',
+			'cascade_save'   => true,
+			'cascade_delete' => true
+		)
+	);
 
-  /**
-   * Object properties
-   *
-   * @var array
-   */
-  protected static $_properties = array(
-    'id',
-    'user_id',
+	/**
+	 * Object properties
+	 *
+	 * @var array
+	 */
+	protected static $_properties = array(
+        'id',
+        'nome',
+        'foto',
+        'sexo',
+        'nascimento',
+        'cidade',
+        'estado',
+        'sicard',
+        'cpf',
+        'identidade',
+        'orgexp',
+        'situacao',
+        'data_situacao',
+        'filiacao',
+        'user_id',
+    );
 
-    // Example fields
-    // 'first_name' => array(
-    //   'validation' => array(
-    //     'null' => true,
-    //     'match_pattern' => array(self::REGEX_NAME)
-    //   )
-    // ),
-
-    // 'last_name' => array(
-    //   'validation' => array(
-    //     'null' => true,
-    //     'match_pattern' => array(self::REGEX_NAME)
-    //   )
-    // ),
-
-    // 'gender',
-    // 'bio',
-    // 'location',
-    // 'searchable',
-    // 'created_at',
-    // 'updated_at'
-  );
-
-  /**
-   * Observer classes to use
-   *
-   * @var array
-   */
-  // protected static $_observers = array(
-  //   'Warden\\Observer_Timestamps'
-  // );
+	/**
+	 * Observer classes to use
+	 *
+	 * @var array
+	 */
+	// protected static $_observers = array(
+	//   'Warden\\Observer_Timestamps'
+	// );
 }
